bb1ca44479a770ef4590726d594d9d8b63c81092
Diego <diego.minchala@outlook.com>
2023-08-03 18:33:57 -0600

    brr

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -7,17 +7,17 @@ const Discounted = () => {
     <section id="recent">
       <div className="container">
         <div className="row">
-         
-            <h2 className="section__title">
+          <h2 className="section__title">
             Discount <span className="purple">Books</span>
+          </h2>
 
-            </h2>
-          
           <div className="books">
             {books
-            .filter(book => book.salePrice > 0).map((book) => (
-              <Book book={book} key={book.id} />
-            ))}
+              .filter((book) => book.salePrice > 0)
+              .slice(0, 8)
+              .map((book) => (
+                <Book book={book} key={book.id} />
+              ))}
           </div>
         </div>
       </div>
