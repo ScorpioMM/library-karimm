4992164cb30c1da9ede2fa27adb6b794e535ab64
Diego <diego.minchala@outlook.com>
2023-08-09 20:32:11 -0600

    thanks

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import LibraryLogo from '../assets/Library.svg'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link } from 'react-router-dom/cjs/react-router-dom';
+
+
 
 
 const Nav = () => {
@@ -69,3 +71,6 @@ return (
 
 export default Nav;
 
+// . . IM trynna change that :) 
+
+// Be more humble. Amen.
