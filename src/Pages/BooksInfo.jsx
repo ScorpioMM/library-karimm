873fce31d91819346a4f74449609cc10c5d981bc
Diego <diego.minchala@outlook.com>
2023-09-07 14:01:37 -0600

    bruh

--- src/Pages/BooksInfo.jsx
+++ src/Pages/BooksInfo.jsx
@@ -63,7 +63,9 @@ const BooksInfo = ({ books }) => {
             <div className="book__selected--top">
               <h2 className="book__selected--title-top">Recommended Books</h2>
             </div>
-
+{
+    books.filter(book => book.rating === 5 && book.id !== id )
+}
           </div>
         </div>
       </main>
